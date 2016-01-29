0e6c345438dfd690f88da794d19d0d5d115ed041
SRenan <renan.sauteraud@gmail.com>
2016-01-29 14:57:52 -0800

    Fix schema update

--- resources/schemas/dbscripts/postgresql/immport-15.32-15.33.sql
+++ resources/schemas/dbscripts/postgresql/immport-15.32-15.33.sql
@@ -1,2 +1,2 @@
 ALTER TABLE immport.neut_ab_titer_result ADD COLUMN virus_strain_preferred VARCHAR(100);
-ALTER TABLE immport.hai ADD COLUMN virus_strain_preferred VARCHAR(100);
+ALTER TABLE immport.hai_result ADD COLUMN virus_strain_preferred VARCHAR(100);

ec45221042731d0e0b9f6f38bd58191a523e8c8a
Matthew Bellew <matthewb@labkey.com>
2017-02-23 17:15:00 -0800

    fix population_cell_number

--- resources/queries/immport/ds_fcs_analyzed_result.sql
+++ resources/queries/immport/ds_fcs_analyzed_result.sql
@@ -8,7 +8,15 @@ PARAMETERS($STUDY VARCHAR DEFAULT NULL)
 SELECT
   subject_accession || '.' || SUBSTRING(study_accession,4) as participantid,
   COALESCE(study_time_collected,9999.0000) AS sequencenum,
-  COALESCE(population_stat_unit_preferred, population_stat_unit_reported) AS cell_number_unit,
+  -- backward compatibility
+  COALESCE(population_statistic_preferred, CAST(population_statistic_reported AS DOUBLE)) AS population_cell_number,
+  CASE
+    WHEN population_statistic_preferred IS NOT NULL THEN population_stat_unit_preferred
+    ELSE population_stat_unit_reported
+  END  AS cell_number_unit,
+  -- fix spelling
+  population_defnition_preferred as population_definition_preferred,
+  population_defnition_reported as population_definition_reported,
   result.*
 FROM fcs_analyzed_result AS result
 WHERE $STUDY IS NULL OR $STUDY = result.study_accession

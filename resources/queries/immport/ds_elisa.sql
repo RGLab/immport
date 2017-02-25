09ae599b700579dbfa9c6dcaa0eee082a5ff77f5
ldashevs <ldashevs@fredhutch.org>
2017-02-25 04:32:43 +0000

    Adding proper "Analyte" column import for ELISA, ELISPOT, and MBAA datasets

--- resources/queries/immport/ds_elisa.sql
+++ resources/queries/immport/ds_elisa.sql
@@ -7,6 +7,7 @@ PARAMETERS($STUDY VARCHAR DEFAULT NULL)
 SELECT
   subject_accession || '.' || SUBSTRING(study_accession,4) as participantid,
   COALESCE(study_time_collected,9999.0000) as sequencenum,
+  COALESCE(analyte_preferred,analyte_reported) AS analyte,
   result.*
 FROM elisa_result AS result
 WHERE $STUDY IS NULL OR $STUDY = result.study_accession

706a47e1712731c9b855e04bdeb758de9cd73ea1
Matthew Bellew <matthewb@labkey.com>
2019-03-21 16:14:09 -0700

    37050: analyte mapping for elisa, elispot, and mbaa

--- resources/queries/immport/ds_elispot.sql
+++ resources/queries/immport/ds_elispot.sql
@@ -7,7 +7,7 @@ PARAMETERS($STUDY VARCHAR DEFAULT NULL)
 SELECT
   subject_accession || '.' || SUBSTRING(study_accession,4) as participantid,
   COALESCE(study_time_collected,9999.0000) as sequencenum,
-  COALESCE(analyte_preferred,analyte_reported) AS analyte,
+  COALESCE((SELECT uniprot_entry FROM lk_analyte WHERE analyte_accession=result.analyte_preferred AND uniprot_entry != '-'),analyte_reported,analyte_preferred) AS analyte,
   result.*
 FROM elispot_result AS result
 WHERE $STUDY IS NULL OR $STUDY = result.study_accession

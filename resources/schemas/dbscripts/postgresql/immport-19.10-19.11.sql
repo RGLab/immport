3bf706abd574cf43adef346d123c3395ef5ef497
Matthew Bellew <matthewb@Labkey.com>
2019-11-05 10:43:18 -0800

    DR32

---
+++ resources/schemas/dbscripts/postgresql/immport-19.10-19.11.sql
@@ -0,0 +1,4 @@
+ALTER TABLE immport.fcs_analyzed_result
+  ALTER COLUMN population_stat_unit_reported TYPE varchar(100);
+ALTER TABLE immport.lk_analyte
+  ALTER COLUMN genetic_nomenclature_id TYPE varchar(100);
